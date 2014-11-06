e6b6fcb4d3382e9583246d57803018c780573143
dezoito <user@linuxmint.linuxmint-domain>
2014-11-06 19:10:02 -0300

    Displays lengths for original and summarized strings Attempts to use portuguese for stop words and sentence summarization

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from flask import render_template, flash, redirect
+import summarize
 from app import app
 from .forms import FormResumo
 
@@ -18,8 +19,19 @@ def form_resumo():
             # flash('Texto digitado:%s ' %
             #       (form.texto.data))
             # return redirect('/index')
-            rc['texto_resumido'] = form.texto.data
+            texto_fixo = """
+            Em uma sala de estar apertada no sul da Turquia, um menino de 13 anos treina para aderir ao grupo autodenominado Estado Islâmico.
 
+Ao chegarmos a sua casa, ele nos recebe como um adolescente comum e feliz: cabelo despenteado, sorriso no rosto e casaco cinza com capuz.
+
+Mas, quando paramos para conversar, ele se dirige ao quarto do lado para mudar de roupa. Volta com uma balaclava preta -capuz que cobre o rosto todo- e roupa com camuflagem militar.
+"""
+            entrada = form.texto.data
+            rc['texto_fixo'] = texto_fixo
+            rc['entrada'] = entrada
+            rc['texto_resumido'] = summarize.summarize_text(entrada, block_sep='\n')
+            rc['l_entrada'] = str(len(entrada))
+            rc['l_resumido'] = str(len(texto_fixo))
 
 
     return render_template('form_resumo.html',
