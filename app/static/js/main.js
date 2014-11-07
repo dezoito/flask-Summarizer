5c03de04db80229c6ed98a9c6f2c72b1af056b54
dezoito <user@linuxmint.linuxmint-domain>
2014-11-07 16:21:46 -0300

    Ajax post function works now (need to return translations in view now)

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,1 +1,27 @@
+jQuery(document).ready(function(){
 
+    /**
+     * requests summarization via ajax
+     */
+    $("#btnAjax").click(function(){
+        var form = $( "form_resumo" );
+        $.ajax({
+                type:"POST",
+                url:"/ajax_resumo",
+                data:form.serialize(),
+                success: function(response){
+                    $( "#txt_resumo" ).val( response );
+                    console.log(response);
+                },
+
+                fail: function(response){
+                    console.log(response);
+                }
+            });
+        });
+
+        // alert($( "texto" ).val());
+        // $.post( "/ajax_resumo", $( "#texto" ).val() {
+        //   $( "#txt_resumo" ).value( data );
+        // });
+});
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, request
 import summarize
 from app import app
 from .forms import FormResumo
@@ -23,6 +23,30 @@ def form_resumo():
     return render_template('form_resumo.html',
                            form=form,
                            rc = rc)
+
+@app.route('/ajax_resumo', methods=['POST'])
+# check: http://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-xv-ajax
+def ajax_resumo():
+    # rc = {}
+    # entrada = request.form['texto']
+    # texto_resumido = summarize.summarize_text(entrada, block_sep='\n')
+    # return texto_resumido
+    return "success"
+
+
+    # if form.validate_on_submit():
+    #         # flash('Texto digitado:%s ' % (form.texto.data))
+    #         # return redirect('/index')
+
+    #         # summarization happens here
+    #         entrada = form.texto.data
+    #         texto_resumido = summarize.summarize_text(entrada, block_sep='\n')
+    #         rc['entrada'] = entrada
+    #         rc['texto_resumido'] = texto_resumido
+
+    # return render_template('ajax_resumo.html',
+    #                        form=form,
+    #                        rc = rc)
 
 
 
