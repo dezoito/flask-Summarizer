1f0c1678c8807af3285ea38bc69cdcc288d7d529
dezoito <user@linuxmint.linuxmint-domain>
2014-11-11 12:53:42 -0300

    Added flask_debug_toolbar (runs only on DEV IPs)

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 from app import views
+
+# the toolbar is only enabled in debug mode:
+app.debug = True
+toolbar = DebugToolbarExtension(app)
