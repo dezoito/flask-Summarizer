56537e827f42d6de223faf13b8e40878bb164def
dezoito <dezoito@users.noreply.github.com>
2015-09-02 17:14:23 -0300

    Added more selenium tests

--- tests/tests_phantomJS.py
+++ tests/tests_phantomJS.py
@@ -10,11 +10,15 @@
 
 import unittest
 import sys
+import time
 sys.path.append('..')
 import config
 import sample_strings
 from flask import Flask
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from app import app
 from utils import print_test_time_elapsed
 
@@ -43,6 +47,16 @@ class StartingTestCase(unittest.TestCase):
         self.driver.find_element_by_id("btnSubmit").click()
         resumo = self.driver.find_element_by_id("txt_resumo").text
         assert "Resuma isso!" in resumo
+
+    @print_test_time_elapsed
+    def test_sample_text(self):
+        self.driver.get(self.baseURL)
+        self.driver.find_element_by_link_text("Sample 1").click()
+        # WebDriverWait(self.driver, 3).until(
+        #     EC.text_to_be_present_in_element((By.ID, "texto"), "a"))
+        self.driver.find_element_by_id("btnSubmit").click()
+        self.assertIn("Um suéter azul.",
+                self.driver.find_element_by_id("txt_resumo").text)
 
 
 if __name__ == '__main__':
