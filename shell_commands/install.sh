ec53f29c4836381d733494a5f283bdb18e96c8f9
dezoito <dezoito@users.noreply.github.com>
2015-09-03 18:13:34 -0300

    Merged PhantomJS install into the main install script minor README correction

--- shell_commands/install.sh
+++ shell_commands/install.sh
@@ -13,6 +13,7 @@ echo "***********************************************"
 sudo apt-get -y update
 # upgrade to sudo 14.04.2 LTS
 # sudo python3.2 get-pip.py
+
 echo "***********************************************"
 echo "--- python/Flask Stuff  (notice version 3) ---"
 echo "***********************************************"
@@ -43,11 +44,36 @@ sudo pip3 install numpy # for textrank
 sudo pip3 install Networkx # for textrank
 sudo pip3 install -U selenium
 
+
 echo "***********************************************"
 echo "--- PhantomJS (for tests)          ---"
 echo "***********************************************"
-sh install_phantomJS.sh
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root" 1>&2
+    exit 1
+fi
 
+PHANTOM_VERSION="phantomjs-1.9.8"
+ARCH=$(uname -m)
+
+if ! [ $ARCH = "x86_64" ]; then
+    $ARCH="i686"
+fi
+
+PHANTOM_JS="$PHANTOM_VERSION-linux-$ARCH"
+
+# sudo apt-get update
+sudo apt-get install build-essential chrpath libssl-dev libxft-dev -y
+sudo apt-get install libfreetype6 libfreetype6-dev -y
+sudo apt-get install libfontconfig1 libfontconfig1-dev -y
+
+cd ~
+wget https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOM_JS.tar.bz2
+sudo tar xvjf $PHANTOM_JS.tar.bz2
+
+sudo mv $PHANTOM_JS /usr/local/share
+sudo ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin
+echo "--- PhantomJS Finished          ---"
 
 
 echo "***********************************************"
